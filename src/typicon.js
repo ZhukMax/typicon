0234b582769f3402f898ada379c83441b70f2e6e
Max Zhuk <zhukmax@ya.ru>
2017-05-12 17:24:03 +0300

    Update typicon.js

--- src/typicon.js
+++ src/typicon.js
@@ -1,1 +1,15 @@
-import default Typicon;
+import React from 'react';
+import 'typicons.font/src/font/typicons.css';
+
+class Typicon extends React.Component {
+  render() {
+    let name = this.props.name,
+        nameOfClass;
+
+    nameOfClass = "typcn typcn-" + name;
+    return (
+      <span class={ nameOfClass }></span>
+    );
+  }
+}
+export default Typicon;
